7f8ef7473307a1058fd24feb8928d03a83254e36
AlexZhaba <programzhava@mail.ru>
2020-10-02 19:15:06 +0300

    Updates

---
+++ precache-manifest.42a7dc774e0c9329f8683e6596048dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f3adb8949419e26cb0a6ebeac3f698",
+    "url": "/DEPCONF/index.html"
+  },
+  {
+    "revision": "c3cd37be9fdfff36c833",
+    "url": "/DEPCONF/static/css/main.bb9ecd12.chunk.css"
+  },
+  {
+    "revision": "9a56e479e0e85d6e2c25",
+    "url": "/DEPCONF/static/js/2.14f77423.chunk.js"
+  },
+  {
+    "revision": "fdb2bbbbb647ba518ef54493c5cba538",
+    "url": "/DEPCONF/static/js/2.14f77423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3cd37be9fdfff36c833",
+    "url": "/DEPCONF/static/js/main.8640bff7.chunk.js"
+  },
+  {
+    "revision": "4a0c0c2ba75089aab7d3",
+    "url": "/DEPCONF/static/js/runtime-main.70d89e85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DEPCONF/static/media/logo.5d5d9eef.svg"
+  }
+]);
